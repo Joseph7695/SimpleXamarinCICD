27b0f4e9e142eaca2487b73070b3676e211f852a
Joseph <jtfq7695@gmail.com>
2021-10-06 12:26:16 +0800

    Add project files.

---
+++ XamarinApp.Test/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace XamarinApp.Test
+{
+	public class AppInitializer
+	{
+		public static IApp StartApp(Platform platform)
+		{
+			if (platform == Platform.Android)
+			{
+				return ConfigureApp
+					.Android
+					//.InstalledApp("com.companyname.xamarinapp") only work on release ? or is mono runtime
+					.ApkFile(@"./XamarinApp/XamarinApp.Android/bin/Debug/com.companyname.xamarinapp.apk")
+					.StartApp(Xamarin.UITest.Configuration.AppDataMode.DoNotClear);
+			}
+
+			return ConfigureApp.iOS.StartApp();
+		}
+	}
+}
